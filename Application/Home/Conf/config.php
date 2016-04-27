c2b366cafb97556b2962d53a3a095f1defe49cbb
YuDaMao <279714573@qq.com>
2016-04-27 10:11:01 +0800

    消除Home，修改数据库

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,10 +1,11 @@
 <?php
 return [
+	'URL_MODEL' => 3,
         'DB_TYPE'               =>  'mysqli',     // 数据库类型
-        'DB_HOST'               =>  'localhost', // 服务器地址
-        'DB_NAME'               =>  'memory',          // 数据库名
-        'DB_USER'               =>  'root',      // 用户名
-        'DB_PWD'                =>  '279714573',          // 密码
+        'DB_HOST'               =>  'rdsy3674506w15suu3r2.mysql.rds.aliyuncs.com', // 服务器地址
+        'DB_NAME'               =>  'r94xo4ict6',          // 数据库名
+        'DB_USER'               =>  'memory',      // 用户名
+        'DB_PWD'                =>  'Jc001122',          // 密码
         'DB_PORT'               =>  '3306',        // 端口
         'DB_PREFIX'             =>  '',    // 数据库表前缀
         'DB_PARAMS'=> array(PDO::ATTR_CASE => \PDO::CASE_NATURAL), //不需要自动转换大小写
